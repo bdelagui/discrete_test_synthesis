86b71754f755fa90e565d9a5f333fe32e2cee621
Apurva Badithela <13437374+abadithela@users.noreply.github.com>
2020-08-10 09:17:43 -0700

    Commit changes

--- run_example.py
+++ run_example.py
@@ -3,7 +3,7 @@ from examples.dynamic_examples import dyn_example1, dyn_example2
 from examples.static_examples import Illustrate
 
 # Uncomment one of the following lines depending on which example you want to run:
-# Easter_Egg_Hunt.run_easter_egg_example()
-dyn_example1.gridworld_example() # This example will have simulations saved in the Dynamic_Obstacles folder
+Easter_Egg_Hunt.run_easter_egg_example()
+# dyn_example1.gridworld_example() # This example will have simulations saved in the Dynamic_Obstacles folder
 
 # Illustrate.static_example()
